18ab9df0eac9889f2965a09cd49b647d7bd48404
YuuShigetani <s2g4t1n2@gmail.com>
2014-11-27 13:05:21 +0900

    bump up version of openmpi

--- chef-repo/site-cookbooks/openmpi/attributes/default.rb
+++ chef-repo/site-cookbooks/openmpi/attributes/default.rb
@@ -1,7 +1,7 @@
 default['openmpi']['package'] = false
 default['openmpi']['debian']  = %w(openmpi-bin libopenmpi-dev)
 
-default['openmpi']['version']   = '1.8.2'
+default['openmpi']['version']   = '1.8.3'
 default['openmpi']['url']       = "http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-#{node['openmpi']['version']}.tar.gz"
 default['openmpi']['prefix']    = '/usr/local'
 default['openmpi']['make_opts'] = []
